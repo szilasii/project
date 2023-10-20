bc8c4c01ac9484fe82a10dec0d8d817d8702d715
szilasii <szilasii78@gmail.com>
2023-10-20 07:59:53 +0200

    add lek

---
+++ pelda/src/lekérdezés/lek.js
@@ -0,0 +1,36 @@
+// Felhasználó ID bekérése a prompt ablakban
+const userId = prompt("Kérem, adja meg a felhasználó ID-jét:");
+
+// Ellenőrzés, hogy valóban van-e megadva ID
+if (userId !== null && userId !== "") {
+    // Felhasználó adatainak lekérése a szerverről
+    fetch(`http://localhost:8000/user/${userId}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((userData) => {
+            // Ellenőrzés, hogy a szerver visszatért-e adatokkal
+            if (userData.length > 0) {
+                // Az adatok megjelenítése a táblázatban
+                const userTable = document.getElementById("user-list");
+
+                userData.forEach((user) => {
+                    const row = userTable.insertRow();
+                    row.innerHTML = `
+                        <td>${user.userID}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>${user.password}</td>
+                        <td>${user.accountNumber}</td>
+                    `;
+                });
+            } else {
+                alert("Nincs találat a megadott ID-re.");
+            }
+        })
+        .catch((error) => {
+            console.error("Hiba történt:", error);
+        });
+} else {
+    alert("Nem adott meg ID-t vagy a műveletet megszakította.");
+}
