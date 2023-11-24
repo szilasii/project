9662c24148174b0e9fffb3ea8c4ab95bd50fe662
szilasii <szilasii78@gmail.com>
2023-11-24 12:37:06 +0100

    jwt-token3

--- pelda/user/router.js
+++ pelda/user/router.js
@@ -1,7 +1,8 @@
-var user_model = require('./user-model')
-var router = require('express').Router()
+var user_model = require('./user-model');
+var router = require('express').Router();
+const auth = require("../auth/auth");
 
-router.get('/user',user_model.getAllUserInfos);
+router.get('/user',auth,user_model.getAllUserInfos);
 router.get('/user/:id',user_model.getUserDataFromId);
 router.post('/user',user_model.regUser);
 router.post('/user/:id/address',user_model.createNewAddress);
