4f0ea66524c7ee53dc1cf0f7b6cd2561a4f0a337
szilasii <szilasii78@gmail.com>
2023-11-30 14:31:45 +0100

    php-auth-2

--- backend/RESTAPI/user.php
+++ backend/RESTAPI/user.php
@@ -12,18 +12,44 @@ if (!isset($params[1])) {
     }
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
-    $sql = "select * from User where userID = ?";
-    $stmt = $pdo->prepare($sql);
+if (isset($params[1])) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+        $sql = "select * from User where userID = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$params[1]]);
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+    return;    
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE' && isset($params[1])) {
+    $stmt = $pdo->prepare('delete from User where userID = ?');
     $stmt->execute([$params[1]]);
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
-return;    
+    return $stmt;
+}
+
+$data = json_decode(file_get_contents('php://input')) !== null ? json_decode(file_get_contents('php://input')) : null;
+
+if ($data) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {   
+        
+        try {
+        $stmt = $pdo->prepare('insert into User values(null,?,?,?,?,null)');
+        $stmt->execute([$data->name,$data->email,$data->password,$data->accountNumber]);
+        } catch(Exception $e) {
+            http_response_code(400);
+            die("Már létező emailcím");   
+        }
+        var_dump($stmt);
+        return $data;   
+    }
+}
+else {
+    http_response_code(400);
+    die("Nem megfelelően kitöltött adat"); 
 }
 
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    
-}
 
